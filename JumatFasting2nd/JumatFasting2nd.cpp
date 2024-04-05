b7695fbac297f81544ccca7d7de65d7107432d25
nopriterbang <novalpriya@gmail.com>
2024-04-05 15:21:06 +0700

    membuat perulangan i kurang dari n

--- JumatFasting2nd/JumatFasting2nd.cpp
+++ JumatFasting2nd/JumatFasting2nd.cpp
@@ -38,6 +38,20 @@ void LinearSearch()
 		// Accept the number to be searched
 		cout << "\nEnter the elements you want search: "; // step 1
 		cin >> item;
+
+		ctr = 0;
+		i = 0; // step 2
+		while (i < n) // step 3
+		{
+			ctr++; 
+			if (arr[i] == item)
+			{
+				cout << "\n" << item << "found at position" << (i + 1) << endl; // step 6
+				break;
+			}
+			i++: // step 4
+		}
+		
 	}while
 
 }
