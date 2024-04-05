7098f21810a60f9d1d9d8aa804b95d2574c64910
nopriterbang <novalpriya@gmail.com>
2024-04-05 14:49:28 +0700

    membuat loop

--- JumatFasting2nd/JumatFasting2nd.cpp
+++ JumatFasting2nd/JumatFasting2nd.cpp
@@ -21,4 +21,9 @@ void input()
 	cout << "\n-------------------\n";
 	cout << " Enter array elements\n";
 	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
